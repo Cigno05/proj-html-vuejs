e6312efc913881e7a3f14207dd76f5bd49f04aff
Cigno05 <paolocalcagni.82@gmail.com>
2024-04-22 12:16:45 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>App <font-awesome-icon :icon="['fas', 'house']" /></h1>
+    <AppHeader />
+    <AppMain />
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue'
+
+
+  export default {
+    components: {
+      AppHeader,
+      AppMain,
+      AppFooter,
+    },
+    
+  }
+</script>
+
+<style lang="scss" >
+@use 'bootstrap';
+@use './style/general';
+
+</style>
